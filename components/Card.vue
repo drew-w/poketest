40f89bfa8f93072b208c88fbecfc132afe8165d1
drew-w <eesnamdoow@gmail.com>
2023-10-30 17:04:17 -0600

    home page finished and working

---
+++ components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <NuxtLink :to="`/pokemon/${pokemon?.id}`">
+      <h2>{{ pokemon?.name }}</h2>
+    </NuxtLink>
+    <NuxtImg
+      :src="pokemon?.sprites.front_default"
+      draggable="false"
+      width="100"
+      height="100"
+      fit="cover"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PokemonList, Pokemon } from '~/types/pokemon'
+
+const { pokelist } = defineProps<{ pokelist: PokemonList }>()
+const { url } = pokelist
+const { data: pokemon } = await useFetch<Pokemon>(`/api/pokemon/1?url=${url}`)
+
+onMounted(async () => {
+  //   console.log(pokemon.value?.name)
+})
+</script>
+
+<style scoped>
+.card {
+  border-radius: 8px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #0c0c0c;
+  margin: 5px;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 150px;
+  height: 200px;
+  background-color: aquamarine;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useDebounceFn } from '@vueuse/core'
+import type { PokemonList } from '~/types/pokemon'
+
+const { data: initialPokemon, error } = await useFetch<PokemonList[] | null>(
+  '/api/list?count=10'
+)
+// store our list of pokemon here
+const pokemonList = ref(initialPokemon)
+const text = ref('')
+
+// dont filter right away
+const handleChange = useDebounceFn((event: Event) => {
+  text.value = (event.target as HTMLInputElement).value
+}, 1000)
+
+// Filters list of pokemon based on user input
+const filteredPokemon = computed(() => {
+  return (pokemonList.value || []).filter(listItem =>
+    listItem.name.includes(text.value)
+  )
+})
+
+//todo REMOVE THIS
+onMounted(async () => {
+  console.log(error)
+})
+</script>
+
+<template>
+  <div>
+    <h2>Home</h2>
+    <div id="app">
+      <input
+        type="text"
+        :value="text"
+        placeholder="Filter"
+        @input="handleChange"
+      />
+      <br />
+      You typed: <b>{{ text }}</b>
+    </div>
+    <section>
+      <div v-if="text === ''" v-for="p in initialPokemon">
+        <Card :pokelist="p" />
+      </div>
+      <div
+        v-else-if="text !== '' && filteredPokemon.length > 0"
+        v-for="p in filteredPokemon"
+      >
+        <Card :pokelist="p" />
+      </div>
+      <div v-else>
+        <p>No results</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+section {
+  flex-wrap: wrap;
+  max-width: 100vw;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
